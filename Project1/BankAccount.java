b043a7d485e9f7ea70c007440eb1ea57dba8df59
bbibeault <bibeaulb@go.stockton.edu>
2022-03-26 21:18:11 -0400

    move on

--- Project1/BankAccount.java
+++ Project1/BankAccount.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import javax.lang.model.util.ElementScanner14;
 import javax.net.ssl.KeyStoreBuilderParameters;
 import javax.sql.rowset.spi.SyncResolver;
+import javax.swing.plaf.synth.SynthSpinnerUI;
 
 public class BankAccount {
     
@@ -41,18 +42,28 @@ public class BankAccount {
         Scanner keyboard = new Scanner (System.in);
         if(bank.getBalance() >= withdrawAmount){
             bank.withdraw(withdrawAmount);
-            // System.out.println(withdrawAmount + " has been withdrawn. Thank you!");
-        }else if(bank.getBalance() < withdrawAmount){
+        }else if(bank.getBalance() < withdrawAmount) {
             System.out.println("You are trying to withdraw more money than you have in your account. Proceeding will result in an overdraft fee of $35.00. Would you like to proceed? Click 1 for Yes or 2 for No.");
-            int Y;
-            int N;
+            int Y = '1';
+            int N = '2';
             Y = keyboard.nextInt();
-                bank.withdraw(withdrawAmount + 35);
+                bank.withdraw( + 35);
             N = keyboard.nextInt();
-            System.out.println("Your transaction has been cancelled.");
-            // running both, looking for a if/then statement?
+            System.out.println("Your transacion has been cancelled.");
         }
     }
+
+            // System.out.println(withdrawAmount + " has been withdrawn. Thank you!");
+        // }else if(bank.getBalance() < withdrawAmount){
+        //     System.out.println("You are trying to withdraw more money than you have in your account. Proceeding will result in an overdraft fee of $35.00. Would you like to proceed? Click 1 for Yes or 2 for No.");
+        //     int Y;
+        //     int N;
+        //     Y = keyboard.nextInt();
+        //         bank.withdraw(withdrawAmount + 35);
+        //     N = keyboard.nextInt();
+        //     System.out.println("Your transaction has been cancelled.");
+            // running both, looking for a if/then statement?
+        // }
 
 
     public void monthlyfee(){
