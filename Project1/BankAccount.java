37db4f289bf81c2848151dbfebd27873ada68ee6
bbibeault <bibeaulb@go.stockton.edu>
2022-03-27 12:21:45 -0400

    overdraft done

--- Project1/BankAccount.java
+++ Project1/BankAccount.java
@@ -58,17 +58,7 @@ public class BankAccount {
             System.out.println("Your transaction has been cancelled");
         }
     }
-
-        // }else if(bank.getBalance() < withdrawAmount) {
-        //     System.out.println("You are trying to withdraw more money than you have in your account. Proceeding will result in an overdraft fee of $35.00. Would you like to proceed? Click 1 for Yes or 2 for No.");
-        //     int Y = '1';
-        //     int N = '2';
-        //     Y = keyboard.nextInt();
-        //         bank.withdraw(withdrawAmount + 35);
-        //     N = keyboard.nextInt();
-        //     System.out.println("Your transacion has been cancelled.");
-        // }
-    }
+}
 
     public void monthlyfee(){
         withdraw(10);
