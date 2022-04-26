4123704ab8f253212675ea6354d2b18f3a755971
bbibeault <bibeaulb@go.stockton.edu>
2022-04-26 17:47:05 -0400

    bad loops

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -67,10 +67,15 @@ public class Arrays {
         char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
+        int i = 0;
+        for (i = 0; i < password.charAt(i); i++); {
+            while (password.charAt(i) == alphabet) {
 
+            }
+        }
         return answer;
 
-        // i genuinely have no idea
+        // loops still get the best of me
     }
 
     // Create a method that will sort an Array of integers from smallest to greatest.
