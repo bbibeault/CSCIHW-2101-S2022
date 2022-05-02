ef5fbb058e12925caf533b9088704891e3a5b7b3
bbibeault <bibeaulb@go.stockton.edu>
2022-05-02 12:31:14 -0400

    awful

--- Project3/Ladder.java
+++ Project3/Ladder.java
@@ -18,5 +18,11 @@ public class Ladder {
         {0,1},
         {0,1}
     };
+
     // setting 0 for glass and 1 for plexiglass?
+    // for loop
+        // for array = 1, =plexiglass, =good
+        // for array = 0, =glass, =bad
+        // incorporate random number generator?
+    // set up variable so R,r = 0, L,l = 1
 }
