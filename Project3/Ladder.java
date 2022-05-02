8e6099918322ee6af4c45b208257dd00a8f02712
bbibeault <bibeaulb@go.stockton.edu>
2022-05-02 12:21:18 -0400

    okay...

--- Project3/Ladder.java
+++ Project3/Ladder.java
@@ -1,5 +1,22 @@
 package Project3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Ladder {
-    
+    int [][] Ladder = {
+        {0,1},
+        {0,1},
+        {1,0},
+        {0,1},
+        {1,0},
+        {1,0},
+        {1,0},
+        {1,0},
+        {0,1},
+        {0,1}
+    };
+    // setting 0 for glass and 1 for plexiglass?
 }
