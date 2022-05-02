864c6c324a1548373238f7394101d64f1bd97f1d
bbibeault <bibeaulb@go.stockton.edu>
2022-05-02 12:10:35 -0400

    progress

--- Project3/Player.java
+++ Project3/Player.java
@@ -2,5 +2,13 @@ package Project3;
 
 public class Player {
     private int lives;
-    
+
+    public void getLives(){
+        lives = 1;
+    }
+
+    public void runDeaths(){
+        int deaths = 1;
+    }
+
 }
