82d6f74e5ed57457aa8e337bf93770d7b05d8de8
bbibeault <bibeaulb@go.stockton.edu>
2022-05-02 20:56:28 -0400

    easy imported

--- Project3/Player.java
+++ Project3/Player.java
@@ -1,13 +1,13 @@
-package Project3;
-
 public class Player {
     private int lives;
 
-    public void getLives(){
-        lives = 1;
-    }
+    // make a Player object with no parameter (default lives = 5)
 
-    public void runDeaths(){
-        int deaths = 1;
-    }
+    // make a player object with lives parameter
+
+    // make a method to get the lives 
+    // return lives (int)
+
+    // make a death method to subtract a life from the lives total (void)
+    
 }
